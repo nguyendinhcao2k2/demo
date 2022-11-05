9950eec817fa3d1c1575efe1dea2ba30d92d921e
caodinh <caococ2002@gmail.com>
2022-11-05 15:25:54 +0700

    ahhi

---
+++ main.js
@@ -0,0 +1,28 @@
+//Khai Bao Bien
+
+// var fullName = "Nguyen Dinh Cao";
+// var age = 23;
+
+// alert(fullName);
+// alert(age);
+
+//In ra man hinh Console
+// console.log('Nguyen Dinh Cao');
+
+//Dung De Xac Nhan
+// confirm('Xac Nhan Ban Du Tuoi');
+
+//Dung nhap gia tri
+// prompt('Moi Nhap Tuoi');
+
+//Chay 1 lan
+// setTimeout(function() {
+//     alert('Thong Bao')
+// }, 1000)
+
+//Chay lien tuc
+// setInterval(function() {
+//     alert('Ahihi');
+// },200)
+
+
